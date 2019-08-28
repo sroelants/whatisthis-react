78df3dd1f0f81a72b959afb4a0986fec55de785b
Sam Roelants <roelants.sam@gmail.com>
2019-08-28 21:46:10 +0200

    Added prism to CodeField component

--- src/CodeField.js
+++ src/CodeField.js
@@ -1,13 +1,26 @@
 import React from "react";
+import Prism from "prismjs";
+import "./prism-monokai.css";
 
-function CodeField({ content }) {
-  return (
-    <div className="codesnippet">
-      <pre className="lang-js">
-        <code class="lang-js codesnippet__content">{content}</code>
-      </pre>
-    </div>
-  );
+class CodeField extends React.Component {
+  constructor(props) {
+    super(props);
+    this.content = props.content;
+  }
+
+  componentDidMount() {
+    Prism.highlightAll();
+  }
+
+  render() {
+    return (
+      <div className="codesnippet">
+        <pre className="lang-js">
+          <code class="lang-js codesnippet__content">{this.content}</code>
+        </pre>
+      </div>
+    );
+  }
 }
 
 export default CodeField;
