1d502a22f4ed57dc2b532d0486398e598143affb
Sam Roelants <roelants.sam@gmail.com>
2019-08-30 00:06:11 +0200

    Added Modal logic in switch statement, added onClick handler (ish...)

--- src/Modal.js
+++ src/Modal.js
@@ -8,28 +8,31 @@ class Modal extends React.Component {
     this.type = props.type;
     this.content = props.content;
     this.closeModal = props.closeHandler;
+    this.explanationHandler = props.explanationHandler;
+    this.buttons = this.buttons.bind(this);
+  }
+
+  buttons(context) {
+    switch (context) {
+      case "answer":
+        return (
+          <div className="modal__options">
+            <ModalButton type="explanation" onClick={this.explanationHandler} />
+            <ModalButton type="next" />
+          </div>
+        );
+      case "explanation":
+        return (
+          <div className="modal__options">
+            <ModalButton type="next" />
+          </div>
+        );
+      default:
+        return null;
+    }
   }
 
   render() {
-    function buttons(context) {
-      if (context === "answer") {
-        return (
-          <div className="modal__options">
-            <ModalButton type="explanation" />
-            <ModalButton type="next" />
-          </div>
-        );
-      } else if (context === "explanation") {
-        return (
-          <div className="modal__options">
-            <ModalButton type="next" />
-          </div>
-        );
-      } else {
-        return null;
-      }
-    }
-
     return (
       <div>
         <div className="mask" />
@@ -40,7 +43,7 @@ class Modal extends React.Component {
             </span>
           </button>
           {this.content}
-          {buttons(this.type)}
+          {this.buttons(this.type)}
         </div>
       </div>
     );
