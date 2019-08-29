b802c01ce444591a59d46c7c8b0c7f5f68dbecaa
Sam Roelants <roelants.sam@gmail.com>
2019-08-29 22:36:50 +0200

    Changed explanation strings to JSX

--- src/questions.js
+++ src/questions.js
@@ -1,3 +1,4 @@
+import React from "react";
 let questions = [
   {
     codesnippet: `
@@ -10,10 +11,14 @@ console.log(this);
     `,
     options: ["window", "undefined", "fun", "console"],
     correct: "window",
-    explanation: `<p>In a global context (ie. when not referenced within the body of a function
-  declaration), <code>this</code> always refers to the global object, in this case <code>window</code>.
-  This remains true whether javascript is running in strict mode or not.</p>
- `
+    explanation: (
+      <p>
+        In a global context (ie. when not referenced within the body of a
+        function declaration), <code>this</code> always refers to the global
+        object, in this case <code>window</code>. This remains true whether
+        javascript is running in strict mode or not.
+      </p>
+    )
   },
   {
     codesnippet: `
@@ -30,11 +35,15 @@ console.log(obj.fun());
     `,
     options: ["window", "undefined", "myFun", "obj"],
     correct: "obj",
-    explanation: `<p><code>this</code> refers to the <em>execution</em> context.
-    It completely disregards the lexical context of where the keyword is used
-    within the program code. In this instance, the function body is run as a 
-    method on the object <code>obj</code>, so the execution context sets <code>
-    this</code> to <code>obj</code></p>`
+    explanation: (
+      <p>
+        <code>this</code> refers to the <em>execution</em> context. It
+        completely disregards the lexical context of where the keyword is used
+        within the program code. In this instance, the function body is run as a
+        method on the object <code>obj</code>, so the execution context sets{" "}
+        <code>this</code> to <code>obj</code>
+      </p>
+    )
   },
   {
     codesnippet: `
@@ -50,12 +59,16 @@ myFun(1,2);
     `,
     options: ["window", "undefined", "myFun", "obj"],
     correct: "window",
-    explanation: `<code>this</code> refers to the <em>execution</em> context of
-    a function. Even though <code>myFun</code> is a reference to the
-    <code>myMethod</code> method on <code>obj</code>, it is <em>executed</em> as a
-    top-level function. Hence, the execution context is set to <code>
-    window</code> (How would this change if we set <code>'use strict;'</code>?)
-    </p>`
+    explanation: (
+      <p>
+        <code>this</code> refers to the <em>execution</em> context of a
+        function. Even though <code>myFun</code> is a reference to the
+        <code>myMethod</code> method on <code>obj</code>, it is{" "}
+        <em>executed</em> as a top-level function. Hence, the execution context
+        is set to <code>window</code> (How would this change if we set{" "}
+        <code>'use strict;'</code>?)
+      </p>
+    )
   },
   {
     codesnippet: `
@@ -71,12 +84,15 @@ mike.greet();
     `,
     options: ["window", "undefined", "mike", "luke"],
     correct: "mike",
-    explanation: `<p>
-    Even though <code>mike</code> doesn't have a <code>greet()</code> method
-    set, it inherits it from the <code>luke</code> prototype. Since <code>this
-    </code> refers to the execution context, not the lexical context (where it
-    is defined), it refers to <code>mike</code>, not <code>luke</code>.
-    </p>`
+    explanation: (
+      <p>
+        Even though <code>mike</code> doesn't have a <code>greet()</code> method
+        set, it inherits it from the <code>luke</code> prototype. Since{" "}
+        <code>this</code> refers to the execution context, not the lexical
+        context (where it is defined), it refers to <code>mike</code>, not{" "}
+        <code>luke</code>.
+      </p>
+    )
   },
   {
     codesnippet: `
@@ -89,17 +105,20 @@ highFives.call("Mike", "Putin");
     `,
     options: ["Mike", "undefined", "Putin", "window"],
     correct: "Mike",
-    explanation: `<p>
-    The <code>.call()</code> method, defined for function objects, allows one to 
-    explicitely pass a value for <code>this</code> as its first argument. Note 
-    that <code>this</code> need not refer to a javascript object, but can be any
-    datatype, even a string or an integer.
-    <em>Fun fact:</em> All function calls, behind the screens, are transformed 
-    by javascript to an invocation of the <code>call()</code> method. Running a 
-    top-level function <code>fun()</code> is translated by javascript to
-    <code>fun.call(window)</code> (or <code>fun.call(undefined)</code>, if we are
-    in strict mode.
-    </p>`
+    explanation: (
+      <p>
+        The <code>.call()</code> method, defined for function objects, allows
+        one to explicitely pass a value for <code>this</code> as its first
+        argument. Note that <code>this</code> need not refer to a javascript
+        object, but can be any datatype, even a string or an integer.
+        <em>Fun fact:</em> All function calls, behind the screens, are
+        transformed by javascript to an invocation of the <code>call()</code>{" "}
+        method. Running a top-level function <code>fun()</code> is translated by
+        javascript to
+        <code>fun.call(window)</code> (or <code>fun.call(undefined)</code>, if
+        we are in strict mode.
+      </p>
+    )
   },
   {
     codesnippet: `
@@ -118,14 +137,20 @@ printEmotionalState("meh.")
     `,
     options: ["mascot", "undefined", "Tony the Tiger", "window"],
     correct: "mascot",
-    explanation: `<p>
-    This one is tricky. <code>printEmotionalState</code> is a top-level function 
-    that is called in a global context, so we would expect <code>this</code> to
-    refer to the <code>window</code> object. However, <code>printEmotionalState
-    </code> invokes <code>mascot.printMood</code> <em>using the <code>call</code>
-    method</em> and binding <code>this</code> back to <code>mascot</code> 
-    explicitely!
-    </p>`
+    explanation: (
+      <p>
+        This one is tricky. <code>printEmotionalState</code> is a top-level
+        function that is called in a global context, so we would expect{" "}
+        <code>this</code> to refer to the <code>window</code> object. However,{" "}
+        <code>printEmotionalState</code> invokes <code>mascot.printMood</code>{" "}
+        <em>
+          using the <code>call</code>
+          method
+        </em>{" "}
+        and binding <code>this</code> back to <code>mascot</code>
+        explicitely!
+      </p>
+    )
   },
   {
     codesnippet: `
@@ -144,19 +169,22 @@ printEmotionalState("meh.")
     `,
     options: ["Bart and Rod", "Lisa and Todd", "Lisa and Rod", "Bart and Todd"],
     correct: "Lisa and Todd",
-    explanation: `<p>
-    This is a trick question that has more to do with constructors than it does
-    with <code>this</code>. In a typical constructor call (that is, a function
-    that is called with the <code>new</code> keyword and returns an object), the
-    the <code>this</code> keyword refers to the newly created object, and is
-    returned by the constructor call. <em>But</em>, if we wanted to, we can 
-    make the constructor return <em>any</em> object we wish by explicitely 
-    adding a <code>return</code> statement returning an object. In this case,
-    the <code>Flanders</code> constructor creates a new Flanders instance with
-    <code>name</code> set to "Rod", but <em>instead</em> returns a different
-    object, with <code>name</code> set to "Todd". The creation of the first 
-    object is essentially for nothing.
-    </p>`
+    explanation: (
+      <p>
+        This is a trick question that has more to do with constructors than it
+        does with <code>this</code>. In a typical constructor call (that is, a
+        function that is called with the <code>new</code> keyword and returns an
+        object), the the <code>this</code> keyword refers to the newly created
+        object, and is returned by the constructor call. <em>But</em>, if we
+        wanted to, we can make the constructor return <em>any</em> object we
+        wish by explicitely adding a <code>return</code> statement returning an
+        object. In this case, the <code>Flanders</code> constructor creates a
+        new Flanders instance with
+        <code>name</code> set to "Rod", but <em>instead</em> returns a different
+        object, with <code>name</code> set to "Todd". The creation of the first
+        object is essentially for nothing.
+      </p>
+    )
   }
 ];
 
